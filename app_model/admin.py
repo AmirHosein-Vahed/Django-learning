acc4fe47d94c83f995af59a4918f12e99a9258e0
AmirHosein-Vahed <vahed.ahv@gmail.com>
2025-04-06 14:55:49 +0330

    Student model created. shirt_size field added to Person model.

--- app_model/admin.py
+++ app_model/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Musician, Album, Student
 
 # Register your models here.
 admin.site.register(Person)
+admin.site.register(Musician)
+admin.site.register(Album)
+admin.site.register(Student)
--- app_model/models.py
+++ app_model/models.py
@@ -2,16 +2,45 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
+    SHIRT_SIZES = {
+        "S": "Small",
+        "M": "Medium",
+        "L": "Large",
+        }
     first_name = models.CharField(max_length=30)
     last_name  = models.CharField(max_length=30)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, default="M")
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name  = models.CharField(max_length=30)
     instrument = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     num_starts = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " - " + str(self.artist)
+
+class Student(models.Model):
+    YEAR_IN_SCHOOL_CHOICES = [
+        ("FR", "Freshman"),
+        ("SO", "Sophomore"),
+        ("JR", "Junior"),
+        ("SR", "Senior"),
+        ("GR", "Graduate"),
+        ]
+    name = models.CharField(max_length=50)
+    year = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES)
+
+    def __str__(self):
+        return self.name + " - " + self.year
